394ed37fecd8919eba174db981e2d607199eabe7
jwonchoi34 <jwc7.choi@gmail.com>
2025-12-07 22:47:21 -0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.title("My First Streamlit Test")
+st.header("This is a header")
+st.subheader("This is a subheader")
+
+st.write("Hello! Streamlit is working.")
+st.write("You can show text, numbers, lists, or anything you want.")
+
+st.write("Here’s a list example:")
